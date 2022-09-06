e82829b240d025718b58d188e85c1c3a5944a86e
Edwin Rivera <edriver11@elpoli.edu.co>
2022-09-06 11:06:25 -0500

    Usando path params

--- main.py
+++ main.py
@@ -2,9 +2,16 @@
 # Iniciar el servidor: uvicorn main:app --reload
 
 from fastapi import FastAPI
+from enum import Enum
 
-app = FastAPI()
+app = FastAPI() # instancia de fastapi
 
-@app.get("/")
-async def root():
+# de la instancia, que decorador (operador http) voy a usar y que path "/"
+@app.get("/") 
+async def root(): # esta funcion maneja los request que van a este path "/"
     return {"message": "Hello cold world!"}
+
+# path params con definicion de tipos usando hint, usar para validaciones =>
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
