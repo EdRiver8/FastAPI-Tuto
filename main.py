835ae3aa87e4d6e4b6c02c72f6d4aa747939f500
Edwin Rivera <edriver11@elpoli.edu.co>
2022-09-06 11:22:48 -0500

    Usando Enum para el path

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ from enum import Enum
 
 app = FastAPI() # instancia de fastapi
 
+# hereda de str (valores deben ser tipo string) y Enum
+class ModelName(str, Enum): 
+    java = "java"
+    php = "php"
+    python = "python"
+
 # de la instancia, que decorador (operador http) voy a usar y que path "/"
 @app.get("/") 
 async def root(): # esta funcion maneja los request que van a este path "/"
@@ -15,3 +21,15 @@ async def root(): # esta funcion maneja los request que van a este path "/"
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.java:
+        return {"model_name": model_name, "message": "Use Spring"}
+
+    if model_name.value == "php":
+        return {"model_name": model_name, "message": "Use Laravel"}
+
+    return {"model_name": model_name, "message": "Use Fastapi 😁"}
+
+
