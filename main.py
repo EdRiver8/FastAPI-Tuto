fe86fc8987e253adc8db835a3dd2310362501aa7
Edwin Rivera <edriver11@elpoli.edu.co>
2022-09-06 12:18:18 -0500

    parametros query opcionales(usando none) u obligatorios sin el none, combinacion entre path y query params

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 from fastapi import FastAPI
 from enum import Enum
+from typing import Union
 
 app = FastAPI() # instancia de fastapi
 
@@ -17,10 +18,10 @@ class ModelName(str, Enum):
 async def root(): # esta funcion maneja los request que van a este path "/"
     return {"message": "Hello cold world!"}
 
-# path params con definicion de tipos usando hint, usar para validaciones =>
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+# # path params con definicion de tipos usando hint, usar para validaciones =>
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: int):
+#     return {"item_id": item_id}
 
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
@@ -33,3 +34,46 @@ async def get_model(model_name: ModelName):
     return {"model_name": model_name, "message": "Use Fastapi 😁"}
 
 
+# query params, no hacen parte del path, estan en key-value, van despues '?' en URl
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+# mostrar solo uno http://127.0.0.1:8000/items?skip=1&limit=1
+@app.get("/items")
+async def read_items(skip: int = 0, limit: int = 10): # var, tipo dato y valor defecto
+    return fake_items_db[skip : skip + limit] #desde skip hasta skip + limit
+
+# Parametros opcionales usando None por defecto, q = quantity
+# http://127.0.0.1:8000/items/1?q=2
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: str, q: Union[str, None] = None):
+#     if q:
+#         return {"item_id": item_id, "q": q}
+#     return {"item_id": item_id}
+
+# http://127.0.0.1:8000/items/foo?short=1
+# http://127.0.0.1:8000/items/foo?short=true
+# http://127.0.0.1:8000/items/foo?short=on
+# http://127.0.0.1:8000/items/foo?short=yes
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: Union[str, None] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+# Multiples parametros por path y query
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: Union[str, None] = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
